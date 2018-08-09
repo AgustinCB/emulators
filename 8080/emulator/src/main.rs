baeb2c2f0b01c9d61832107f3e6c0b05a92abc95
Agustin Chiappe Berrini <jnieve@gmail.com>
2018-08-08 22:38:49 -0400

    Move disassemble implementation
    Now everything is in one place :heart:

--- 8080/emulator/src/main.rs
+++ 8080/emulator/src/main.rs
@@ -3,8 +3,29 @@ extern crate emulator_space_invaders;
 use emulator_space_invaders::cpu::{Cpu, Instruction, ROM_MEMORY_LIMIT};
 use emulator_space_invaders::timer::Timer;
 use std::env::args;
+use std::cmp::min;
 use std::fs::File;
 use std::io::Read;
+
+const USAGE: &'static str = "Usage: disassembler-8080 [run|disassemble] [file]";
+
+fn get_instructions(bytes: [u8; ROM_MEMORY_LIMIT]) -> Vec<(u16, Instruction)> {
+    let mut result = Vec::with_capacity(bytes.len());
+    let mut pass = 0;
+    let mut pc: u16 = 0;
+    for index in 0..bytes.len() {
+        if pass == 0 {
+            let i = Instruction::from_bytes(&bytes[index..min(index+3, bytes.len())]);
+            let instruction_size = i.size();
+            pass = instruction_size - 1;
+            result.push((pc, i));
+            pc += instruction_size as u16;
+        } else {
+            pass -= 1;
+        }
+    }
+    result
+}
 
 fn read_file(file_name: &str) -> std::io::Result<[u8; ROM_MEMORY_LIMIT]> {
     let metadata = std::fs::metadata(file_name)?;
@@ -37,11 +58,25 @@ fn start_game(memory: [u8; ROM_MEMORY_LIMIT]) {
     }
 }
 
+fn disassemble(memory: [u8; ROM_MEMORY_LIMIT]) {
+    let instructions = get_instructions(memory);
+    for (pc,instruction) in &instructions {
+        println!("{:04x} {}", pc, instruction.to_string());
+    }
+}
+
 fn main() {
     let args: Vec<String> = args().collect();
-    if args.len() != 2 {
-        panic!("Usage: disassembler-8080 [file]")
+    if args.len() != 3 {
+        panic!(USAGE);
     }
-    let memory = read_file(&args[1]).unwrap();
-    start_game(memory);
+    let memory = read_file(&args[2]).unwrap();
+
+    if args[2] == "run" {
+        start_game(memory);
+    } else if args[2] == "disassemble" {
+        disassemble(memory);
+    } else {
+        panic!(USAGE);
+    }
 }
