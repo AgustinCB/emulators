d40747cd781ef0909ddcfcae744a29e12cc407bb
AgustinCB <jnieve@gmail.com>
2018-08-21 09:50:12 -0400

    Sketch of the cpu abstraction
    Created a first draft of it.
    
    A couple concerns: I had to make some methods more visible than
    they originally were. _I think_ that's ok. Also, the
    Instruction is not object safe. Not sure if that'll byte me in
    the ass later.
    
    Maybe I can make it so by requiring an implementation of From?

---
+++ cpu/src/lib.rs
@@ -0,0 +1,60 @@
+extern crate failure;
+
+use failure::Fail;
+
+pub trait MemoryAddressWidth {}
+
+impl MemoryAddressWidth for u8 {}
+impl MemoryAddressWidth for u16 {}
+impl MemoryAddressWidth for u32 {}
+impl MemoryAddressWidth for u64 {}
+
+pub enum Cycles {
+    Single(u8),
+    Conditional { not_met: u8, met: u8 },
+}
+
+pub trait InputDevice {
+    fn read(&mut self) -> u8;
+}
+
+pub trait OutputDevice {
+    fn write(&mut self, byte: u8);
+}
+
+pub trait Instruction<W: MemoryAddressWidth> {
+    fn from_bytes(bytes: &[W]) -> Self;
+    fn size(&self) -> u8;
+    fn get_cycles(&self) -> Cycles;
+}
+
+pub trait Cpu<W: MemoryAddressWidth, I: Instruction<W> + ToString, E: Fail> {
+    fn execute(&mut self) -> Result<u8, E> {
+        let instruction = I::from_bytes(self.get_next_instruction_bytes());
+        if !self.can_run(&instruction) {
+            return Ok(0);
+        }
+        println!("{}", instruction.to_string());
+        self.increase_pc(instruction.size());
+        let cycles = self.get_cycles_for_instruction(&instruction);
+        self.execute_instruction(instruction)?;
+        Ok(cycles)
+    }
+
+    fn get_cycles_for_instruction(&self, instruction: &I) -> u8 {
+        match instruction.get_cycles() {
+            Cycles::Single(cycles) => cycles,
+            Cycles::Conditional { not_met, met } =>
+                self.get_cycles_from_condition(instruction, not_met, met),
+        }
+    }
+
+    fn get_cycles_from_condition(&self, instruction: &I, not_met: u8, met: u8) -> u8;
+    fn can_run(&self, instruction: &I) -> bool;
+    fn increase_pc(&mut self, steps: u8);
+    fn get_next_instruction_bytes(&self) -> &[W];
+    fn execute_instruction(&mut self, instruction: I) -> Result<(), E>;
+    fn is_done(&self) -> bool;
+    fn add_input_device(&mut self, id: u8, device: Box<InputDevice>);
+    fn add_output_device(&mut self, id: u8, device: Box<OutputDevice>);
+}
