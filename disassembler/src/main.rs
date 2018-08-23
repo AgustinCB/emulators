22facbf20e7b573624ed47032e052335156c5a48
AgustinCB <jnieve@gmail.com>
2018-08-23 13:17:12 -0400

    Generalize disassembler
    Work on the disassembler to support multiple CPUs.
    
    This assumes that all cpus are 8 bits, an assumption that soon
    or later I'll have to discard. But good for now.

--- disassembler/src/main.rs
+++ disassembler/src/main.rs
@@ -1,9 +1,9 @@
 extern crate cpu;
-extern crate failure;
+#[macro_use] extern crate failure;
 extern crate intel8080cpu;
 extern crate mos6502cpu;
 
-use cpu::{Instruction, MemoryAddressWidth};
+use cpu::Instruction;
 use failure::{Error, Fail};
 use mos6502cpu::Mos6502Instruction;
 use intel8080cpu::Intel8080Instruction;
@@ -11,6 +11,12 @@ use std::cmp::min;
 use std::env::args;
 use std::fs::File;
 use std::io::Read;
+
+#[derive(Debug, Fail)]
+enum DisassemblerError {
+    #[fail(display = "unimplemented cpu: {}", name)]
+    InvalidCpu { name: String },
+}
 
 // This is an arbitrarily chosen number. We either need RFC 2000 or something else that I dunno yet
 const ROM_MEMORY_LIMIT: usize = 8192;
@@ -21,18 +27,27 @@ Disassemble a binary file for an old cpu. So far, supports only:
 
 - MOS 6502";
 
-fn get_instructions(bytes: [u8; ROM_MEMORY_LIMIT]) -> Result<Vec<(u16, Mos6502Instruction)>, Error> {
-    let mut result = Vec::with_capacity(bytes.len());
+fn get_instructions_for_cpu(cpu: &str, bytes: [u8; ROM_MEMORY_LIMIT])
+    -> Result<Vec<(u16, Box<ToString>)>, Error> {
+    match cpu {
+        "mos6502" => get_instructions::<Mos6502Instruction>(bytes),
+        _ => Err(Error::from(DisassemblerError::InvalidCpu { name: String::from(cpu) })),
+    }
+}
+
+fn get_instructions<I: 'static + Instruction + ToString + From<Vec<u8>>>(bytes: [u8; ROM_MEMORY_LIMIT])
+    -> Result<Vec<(u16, Box<ToString>)>, Error> {
+    let mut result: Vec<(u16, Box<ToString>)> = Vec::with_capacity(bytes.len());
     let mut pass = 0;
     let mut pc: u16 = 0;
     for index in 0..bytes.len() {
         if pass == 0 {
             let i =
-                Mos6502Instruction::from(
+                I::from(
                     bytes[index..min(index+3, bytes.len())].to_vec());
             let instruction_size = i.size()?;
             pass = instruction_size - 1;
-            result.push((pc, i));
+            result.push((pc, Box::new(i)));
             pc += instruction_size as u16;
         } else {
             pass -= 1;
@@ -49,7 +64,7 @@ fn read_file(file_name: &str) -> std::io::Result<[u8; ROM_MEMORY_LIMIT]> {
 }
 
 fn disassemble(memory: [u8; ROM_MEMORY_LIMIT]) -> Result<(), Error> {
-    let instructions = get_instructions(memory)?;
+    let instructions = get_instructions_for_cpu("mos6502",memory)?;
     for (pc,instruction) in &instructions {
         println!("{:04x} {}", pc, instruction.to_string());
     };
