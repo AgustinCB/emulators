9fc2b791bf00d8696c9ab91f49b63590f046df9d
Agustin Chiappe Berrini <jnieve@gmail.com>
2018-08-30 08:19:43 -0400

    Fix edge case
    What happens:
    
    If initialization of the console that runs the Space invaders takes too long, the interruption will be fired before any instruction is ran.
    
    This is problematic because the RST instruction pushes the current PC to `sp-1` and `sp-2`. If `sp` is zero, that causes an overflow.

--- intel8080cpu/src/intel8080cpu.rs
+++ intel8080cpu/src/intel8080cpu.rs
@@ -68,7 +68,7 @@ pub(crate) struct RegisterSet {
 
 impl RegisterSet {
     pub(crate) fn new() -> RegisterSet {
-        RegisterSet { a: 0, b: 0, c: 0, d: 0, e: 0, h: 0, l: 0, sp: 0 }
+        RegisterSet { a: 0, b: 0, c: 0, d: 0, e: 0, h: 0, l: 0, sp: 0xffff }
     }
 }
 
