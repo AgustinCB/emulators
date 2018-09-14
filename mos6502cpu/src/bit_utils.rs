d8857b5b95c3a7d7e00c0564f33a10b7c4ad2403
AgustinCB <jnieve@gmail.com>
2018-09-14 11:58:48 -0400

    Document possible dedup

--- mos6502cpu/src/bit_utils.rs
+++ mos6502cpu/src/bit_utils.rs
@@ -4,6 +4,7 @@ pub(crate) fn two_complement(value: u8) -> u8 {
 }
 
 #[inline]
+// TODO: This function is in three places already. Lets abstract it
 pub(crate) fn two_bytes_to_word(high_byte: u8, low_byte: u8) -> u16 {
     (high_byte as u16) << 8 | (low_byte as u16)
 }
