15a5a38755ab095585dd7a1930455e22800c5e93
Agustin Chiappe Berrini <jnieve@gmail.com>
2018-09-18 11:13:32 -0400

    Sketching the lexer

--- intel8080_assembler/src/lib/mod.rs
+++ intel8080_assembler/src/lib/mod.rs
@@ -1,6 +1,7 @@
 extern crate intel8080cpu;
 
 use intel8080cpu::{Location, Intel8080Instruction};
+use std::io::{Read, Result};
 
 struct Label(String);
 
@@ -20,4 +21,32 @@ pub(crate) enum Intel8080Expression {
     Instruction(Intel8080Instruction),
     LabelDefinition(Label),
     DataDefinition { label: Label, value: u16 },
+}
+
+pub struct Lexer<T: Read> {
+    source: T,
+    tokens: Vec<Intel8080AssemblerToken>,
+}
+
+impl<T: Read> Lexer<T> {
+    pub fn new(source: T) -> Lexer<T> {
+        Lexer {
+            source,
+            tokens: Vec::new(),
+        }
+    }
+
+    fn scan_token(&mut self) -> Result<()> {
+        let mut buffer: [u8; 1] = [0; 1];
+        self.source.read_exact(&mut buffer)?;
+        let token = match buffer[0] as char {
+            ':' => Intel8080AssemblerToken::Colon,
+            ',' => Intel8080AssemblerToken::Comma,
+            '+' => Intel8080AssemblerToken::Plus,
+            '-' => Intel8080AssemblerToken::Minus,
+            _ => panic!("Meh"),
+        };
+        self.tokens.push(token);
+        Ok(())
+    }
 }
