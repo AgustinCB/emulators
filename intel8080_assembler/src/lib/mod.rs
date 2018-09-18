b067abbf03b61ba6ead7cd325178feff11d1f4c4
Agustin Chiappe Berrini <jnieve@gmail.com>
2018-09-18 14:24:10 -0400

    Scan the SP register

--- intel8080_assembler/src/lib/mod.rs
+++ intel8080_assembler/src/lib/mod.rs
@@ -2,7 +2,7 @@ extern crate failure;
 extern crate intel8080cpu;
 
 use failure::Error;
-use intel8080cpu::{Location, Intel8080Instruction};
+use intel8080cpu::{Intel8080Instruction, Location, RegisterType};
 use std::io::{Bytes, Read};
 use std::iter::Peekable;
 
@@ -51,6 +51,13 @@ impl<R: Read> Lexer<R> {
         let token = match input {
             c if c.is_whitespace() => None,
             'A' | 'B' | 'C' | 'D' | 'E' | 'H' | 'L' | 'M' => self.maybe_scan_location(input)?,
+            'S' => {
+                let res = self.maybe_scan_sp_register()?;
+                if let Some(_) = res {
+                    self.source.next();
+                }
+                res
+            },
             ':' => Some(Intel8080AssemblerToken::Colon),
             ',' => Some(Intel8080AssemblerToken::Comma),
             '+' => Some(Intel8080AssemblerToken::Plus),
@@ -63,11 +70,28 @@ impl<R: Read> Lexer<R> {
         Ok(())
     }
 
+    #[inline]
     fn maybe_scan_location(&mut self, input: char) -> Result<Option<Intel8080AssemblerToken>, Error> {
         if let Some(Ok(ref c)) = self.source.peek() {
             if (*c as char).is_whitespace() || (*c as char) == ',' {
                 let register_string = format!("{}", input);
                 Ok(Some(Intel8080AssemblerToken::DataStore(Location::from(&register_string)?)))
+            } else {
+                Ok(None)
+            }
+        } else {
+            Ok(None)
+        }
+    }
+
+    #[inline]
+    fn maybe_scan_sp_register(&mut self) -> Result<Option<Intel8080AssemblerToken>, Error> {
+        let future = self.source.peek();
+        if let Some(Ok(ref c)) = future {
+            if (*c as char) == 'P' {
+                Ok(Some(Intel8080AssemblerToken::DataStore(Location::Register {
+                    register: RegisterType::Sp,
+                })))
             } else {
                 Ok(None)
             }
