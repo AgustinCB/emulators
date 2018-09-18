5e25dc9c62267b701d980112dc782141a306dd74
Agustin Chiappe Berrini <jnieve@gmail.com>
2018-09-18 16:25:22 -0400

    Scan a number
    Only if it starts with 0-9, though. For hexadecimal numbers
    that start with A-F, I'll have to do something else.

--- intel8080_assembler/README.md
+++ intel8080_assembler/README.md
@@ -1,22 +1,24 @@
 # Intel 8080 assembler
 
-Very simple assembler for the Intel 8080 CPU.
+Very simple (and therefore stupid) assembler for the Intel 8080 CPU.
 
 ## Grammar
 
 ```$xslt
-program         → ( dataDefinition | labelDefinition | instruction ) * ;
-instruction     → instructionCode
-                | instructionCode argument
-                | instructionCode argument "," argument ;
-dataDefinition  → label "EQU" number ;
-labelDefinition → label ":" ;
-argument        → number
-                | dataStore ;
-number          → numberLiteral
-                | ( label | numberLiteral ) ( "+" | "-" ) ( label | numberLiteral ) ;
-instructionCode → [A-Z]{2,3} ;
-label           → [A-Z_]+ ;
-numberLiteral   → [0-9]+ ( "H" )? ;
-dataStore       → "A" | "B" | "C" | "D" | "E" | "H" | "L" | "M" | "P" | "SP" ;
+program             → ( dataDefinition | labelDefinition | instruction ) * ;
+instruction         → instructionCode
+                    | instructionCode argument
+                    | instructionCode argument "," argument ;
+dataDefinition      → label "EQU" number ;
+labelDefinition     → label ":" ;
+argument            → number
+                    | dataStore ;
+number              → numberLiteral
+                    | ( label | numberLiteral ) ( "+" | "-" ) ( label | numberLiteral ) ;
+instructionCode     → [A-Z]{2,3} ;
+label               → [A-Z_]+ ;
+numberLiteral       → decimalNumber | hexadecimalNumber ;
+decimalNumber       → [0-9]+ ;
+hexadecimalNumber   → [0-9A-Fa-f]+ ( "H" )? ;
+dataStore           → "A" | "B" | "C" | "D" | "E" | "H" | "L" | "M" | "P" | "SP" ;
 ```
--- intel8080_assembler/src/lib/mod.rs
+++ intel8080_assembler/src/lib/mod.rs
@@ -14,6 +14,8 @@ pub enum AssemblerError {
     UnexpectedCharacter {
         c: char,
     },
+    #[fail(display = "THERE IS SOMETHING VERY WRONG DUDE")]
+    UndefinedError,
 }
 
 pub(crate) enum Intel8080AssemblerToken {
@@ -47,10 +49,10 @@ impl<R: Read> Lexer<R> {
         }
     }
 
-    fn scan_tokens(&mut self) -> Result<(), Error> {
+    pub fn scan_tokens(&mut self) -> Result<(), Error> {
         while let Some(i) = self.source.next() {
             let input = i? as char;
-            self.scan_token(input);
+            self.scan_token(input)?;
         }
         Ok(())
     }
@@ -58,6 +60,7 @@ impl<R: Read> Lexer<R> {
     fn scan_token(&mut self, input: char) -> Result<(), Error> {
         let token = match input {
             c if c.is_whitespace() => Ok(None),
+            c if c.is_digit(10) => self.maybe_scan_number(input),
             'A' | 'B' | 'C' | 'D' | 'E' | 'H' | 'L' | 'M' | 'P' => self.maybe_scan_location(input),
             'S' => self.maybe_scan_sp_register(),
             ':' => Ok(Some(Intel8080AssemblerToken::Colon)),
@@ -73,14 +76,34 @@ impl<R: Read> Lexer<R> {
     }
 
     #[inline]
+    fn maybe_scan_number(&mut self, first_digit: char)-> Result<Option<Intel8080AssemblerToken>, Error> {
+        let mut number_string = format!("{}", first_digit);
+        while self.check(|c| c.is_digit(16)) {
+            let digit = self.source.next().unwrap()? as char;
+            number_string.push(digit);
+        }
+        let radix = if self.check(|c| c == 'H') { 16 } else { 10 };
+        let number = u16::from_str_radix(&number_string, radix)?;
+        if self.is_at_end_of_statement() {
+            Ok(Some(Intel8080AssemblerToken::Number(number)))
+        } else if let Some(Ok(c)) = self.source.peek() {
+            Err(Error::from(AssemblerError::UnexpectedCharacter { c: (*c) as char }))
+        } else {
+            Err(Error::from(AssemblerError::UndefinedError))
+        }
+    }
+
+    #[inline]
+    fn at_end_of_statement(&mut self) -> bool {
+        self.source.peek().is_none() ||
+            self.check(|c| c.is_whitespace() || c == ',' || c == '+' || c == '-')
+    }
+
+    #[inline]
     fn maybe_scan_location(&mut self, input: char) -> Result<Option<Intel8080AssemblerToken>, Error> {
-        if let Some(Ok(ref c)) = self.source.peek() {
-            if (*c as char).is_whitespace() || (*c as char) == ',' {
-                let register_string = format!("{}", input);
-                Ok(Some(Intel8080AssemblerToken::DataStore(Location::from(&register_string)?)))
-            } else {
-                Ok(None)
-            }
+        if self.check(|c| c.is_whitespace() || c == ',') {
+            let register_string = format!("{}", input);
+            Ok(Some(Intel8080AssemblerToken::DataStore(Location::from(&register_string)?)))
         } else {
             Ok(None)
         }
@@ -88,8 +111,8 @@ impl<R: Read> Lexer<R> {
 
     #[inline]
     fn maybe_scan_sp_register(&mut self) -> Result<Option<Intel8080AssemblerToken>, Error> {
-        if let Some(()) = self.maybe_scan_letter('P')? {
-            self.source.next();
+        if self.check(|c| c == 'P') {
+            self.source.next().unwrap()?;
             Ok(Some(Intel8080AssemblerToken::DataStore(Location::Register {
                 register: RegisterType::Sp,
             })))
@@ -99,16 +122,12 @@ impl<R: Read> Lexer<R> {
     }
 
     #[inline]
-    fn maybe_scan_letter(&mut self, needle: char) -> Result<Option<()>, Error> {
+    fn check<F: Fn(char) -> bool>(&mut self, filter: F) -> bool {
         let future = self.source.peek();
         if let Some(Ok(ref c)) = future {
-            if (*c as char) == needle {
-                Ok(Some(()))
-            } else {
-                Ok(None)
-            }
+            filter(*c as char)
         } else {
-            Ok(None)
+            false
         }
     }
 }
